e274b2ef9274f7e3486b81e7becf0023e1f3f576
Huynh Nguyen <huynhnguyen@Huynhs-MacBook-Pro.local>
2025-11-07 13:12:43 +0700

    add devIndicators

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 const nextConfig: NextConfig = {
   /* config options here */
   outputFileTracingRoot: path.join(__dirname, "../../"),
+  /*Uncomment it on production environment*/
+  //devIndicators: false,
 };
 
 export default nextConfig;
